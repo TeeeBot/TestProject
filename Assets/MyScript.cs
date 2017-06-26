604dea45e5e0a5e12c0a25555a94b0d8fefd88f7
Tim Campbell <tc.campbell@gmail.com>
2017-06-26 18:21:55 +1000

    Added something Else

--- Assets/MyScript.cs
+++ Assets/MyScript.cs
@@ -7,6 +7,7 @@ public class MyScript : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		print ("You found me fool!");
+		print ("Something here changed");
 	}
 	
 	// Update is called once per frame
