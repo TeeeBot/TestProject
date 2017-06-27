e0a4b99139eb56e003c57df100260dace6f4d6cd
Tim Campbell <tc.campbell@gmail.com>
2017-06-27 20:14:22 +1000

    Added TestScript and tweaked MyScript on iMac

--- Assets/Scripts/MyScript.cs
+++ Assets/Scripts/MyScript.cs
@@ -9,6 +9,7 @@ public class MyScript : MonoBehaviour {
 		print ("You found me fool!");
 		print ("Something here changed");
 		Debug.Log ("Added on the Macbook");
+		Debug.Log ("And another lil sum summit");
 	}
 	
 	// Update is called once per frame
